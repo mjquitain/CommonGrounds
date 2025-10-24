79d9c7783379a46fd984616f3c1564a78dd56ef9
Mykyla Jaimie Valenzuela <mjquitain5@gmail.com>
2025-10-25 03:03:31 +0800

    Reorder and update imports in main.tsx
    Moved Mantine styles import before custom styles and reordered other imports for clarity. This helps ensure Mantine styles are loaded before application-specific styles.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,14 @@
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
-import './styles.css'
-import reportWebVitals from './reportWebVitals.ts'
 import { MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css'
+import reportWebVitals from './reportWebVitals.ts'
+import './styles.css'
 
 // Create a new router instance
 const router = createRouter({
